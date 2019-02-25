9b878771f945ee2bbd83cdae0d49d846b5490d24
Paul Florence <perso@florencepaul.com>
2019-02-25 19:53:14 +0100

    2018ify the code

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,12 @@
 #![allow(unused_imports)]
 #![no_main] //  Don't use the Rust standard bootstrap. We will provide our own.
 #![no_std] //  Don't use the Rust standard library. We are building a binary that can run on its own.
-#[macro_use]
-extern crate cortex_m; //  Low-level functions for ARM Cortex-M3 processor in STM32 Blue Pill.
-extern crate arrayvec;
-extern crate cortex_m_rt; //  Startup and runtime functions for ARM Cortex-M3.
-extern crate cortex_m_semihosting; //  Debug console functions for ARM Cortex-M3.
-extern crate embedded_hal;
-extern crate librobot;
-extern crate nb;
-extern crate numtoa;
-extern crate panic_semihosting; //  Panic reporting functions, which transmit to the debug console.
-extern crate pid_control;
-extern crate qei;
 
-extern crate stm32f1xx_hal as bluepill_hal; //  Hardware Abstraction Layer (HAL) for STM32 Blue Pill.
+use stm32f1xx_hal as hal; //  Hardware Abstraction Layer (HAL) for STM32 Blue Pill.
 
 use stm32f1xx_hal::stm32f1::stm32f103::interrupt;
 
-use crate::bluepill_hal::stm32 as f103;
+use crate::hal::stm32 as f103;
 
 use cortex_m_rt::{entry, exception};
 
@@ -30,13 +18,14 @@ use core::fmt::Write;
 
 use cortex_m::Peripherals as CortexPeripherals;
 
-use crate::bluepill_hal::delay::Delay;
-use crate::bluepill_hal::prelude::*; //  Define HAL traits.
-use crate::bluepill_hal::pwm_input::*;
-use crate::bluepill_hal::qei::Qei;
-use crate::bluepill_hal::serial::Serial;
-use crate::bluepill_hal::time::Hertz;
+use panic_semihosting;
+
 use crate::f103::Peripherals;
+use crate::hal::delay::Delay;
+use crate::hal::prelude::*; //  Define HAL traits.
+use crate::hal::qei::Qei;
+use crate::hal::serial::Serial;
+use crate::hal::time::Hertz;
 
 use cortex_m::asm;
 use cortex_m_semihosting::hio; //  For displaying messages on the debug console. //  Clocks, flash memory, GPIO for the STM32 Blue Pill.
@@ -61,8 +50,6 @@ fn main() -> ! {
 
     let mut dbg = bluepill.DBG;
 
-    //cortex.NVIC.enable(stm32f1xx_hal::stm32::Interrupt::TIM3);
-
     // Config des horloges
     let mut rcc = bluepill.RCC.constrain();
     let mut flash = bluepill.FLASH.constrain();
@@ -76,75 +63,10 @@ fn main() -> ! {
         .pclk2(72.mhz())*/
         .freeze(&mut flash.acr);
 
-    let _delay = Delay::new(cortex.SYST, clocks);
-
     // Config du GPIO
     let mut gpiob = bluepill.GPIOB.split(&mut rcc.apb2);
-    let _gpioa = bluepill.GPIOA.split(&mut rcc.apb2);
 
-    let pb10 = gpiob.pb10.into_alternate_push_pull(&mut gpiob.crh);
-    let pb11 = gpiob.pb11.into_floating_input(&mut gpiob.crh);
-
-    let pb4 = gpiob.pb4.into_alternate_open_drain(&mut gpiob.crl);
-    let pb5 = gpiob.pb5.into_alternate_open_drain(&mut gpiob.crl);
-
-    let timer = bluepill.TIM3.pwm_input(
-        (pb4, pb5),
-        &mut rcc.apb1,
-        &mut afio.mapr,
-        &mut dbg,
-        &clocks,
-        Configuration::Frequency(1.mhz()),
-    );
-
-    let serial = Serial::usart3(
-        bluepill.USART3,
-        (pb10, pb11),
-        &mut afio.mapr,
-        115_200.bps(),
-        clocks,
-        &mut rcc.apb1,
-    );
-
-    let (mut ser, _) = serial.split();
-
-    let mut buffer_freq = [0u8; 64];
-    let mut buffer_duty = [0u8; 64];
-
-    let str1 = b"Freq : ";
-    //let str1 = b'A';
-    let sep = b" | ";
-    let str2 = b"Duty : ";
-    let end = b"\n\r";
-
-    loop {
-        match (
-            timer.read_duty(ReadMode::Instant),
-            timer.read_frequency(ReadMode::Instant, &clocks),
-        ) {
-            (Ok((duty, period)), Ok(freq)) => {
-                let percent = 100.0 * (duty as f32 / period as f32);
-                let duty_indices = (percent as u16).numtoa(10, &mut buffer_duty);
-                let freq_indices = freq.0.numtoa(10, &mut buffer_freq);
-
-                for b in str1
-                    .iter()
-                    .chain(freq_indices.iter())
-                    .chain(sep.iter())
-                    .chain(str2.iter())
-                    .chain(duty_indices.iter())
-                    .chain(end.iter())
-                {
-                    block!(ser.write(*b)).expect("Failed to send data");
-                }
-            }
-            _ => {
-                for b in b"Invalid Frequency\n" {
-                    block!(ser.write(*b)).expect("Failed to send data");
-                }
-            }
-        }
-    }
+    loop {}
 }
 
 //  For any hard faults, show a message on the debug console and stop.
