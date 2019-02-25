4c182274158efe6516afdcaad3160ce7255df585
Paul Florence <perso@florencepaul.com>
2019-02-25 22:55:07 +0100

    Clean project

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,48 @@
-//! Blink the LED (connected to Pin PC 13) on and off with 1 second interval.
-
-#![allow(unused_imports)]
-#![no_main] //  Don't use the Rust standard bootstrap. We will provide our own.
-#![no_std] //  Don't use the Rust standard library. We are building a binary that can run on its own.
-
-use stm32f1xx_hal as hal; //  Hardware Abstraction Layer (HAL) for STM32 Blue Pill.
-
-use stm32f1xx_hal::stm32f1::stm32f103::interrupt;
-
-use crate::hal::stm32 as f103;
-
-use cortex_m_rt::{entry, exception};
-
-use nb::block;
-
-use core::fmt::Write;
-
-use cortex_m::Peripherals as CortexPeripherals;
-
-use panic_semihosting;
+#![no_main]
+#![no_std]
 
 use crate::f103::Peripherals;
-use crate::hal::delay::Delay;
-use crate::hal::prelude::*; //  Define HAL traits.
-use crate::hal::qei::Qei;
-use crate::hal::serial::Serial;
-use crate::hal::time::Hertz;
-
-use cortex_m::asm;
-use cortex_m_semihosting::hio; //  For displaying messages on the debug console. //  Clocks, flash memory, GPIO for the STM32 Blue Pill.
-
-use cortex_m_rt::ExceptionFrame; //  Stack frame for exception handling.
-
-use qei::QeiManager;
-
-use librobot::navigation::{Motor, PIDParameters};
-use librobot::units::MilliMeter;
-
-use numtoa::NumToA;
-
-// Black Pill starts execution at function main().
-// interrupt!(TIM3, tim3);
+use crate::hal::stm32 as f103;
+use cortex_m::Peripherals as CortexPeripherals;
+use cortex_m_rt::entry;
+#[allow(unused_imports)]
+use panic_semihosting;
+use stm32f1xx_hal as hal;
+use w5500::*;
+mod robot;
+use crate::robot::init_peripherals;
+use librobot::transmission::{
+    eth::{init_eth, SOCKET_UDP},
+    io::IO,
+    Jsonizable,
+};
 
 #[entry]
 fn main() -> ! {
-    let bluepill = Peripherals::take().unwrap();
+    let chip = Peripherals::take().unwrap();
     let cortex = CortexPeripherals::take().unwrap();
-    let _debug_out = hio::hstdout().unwrap();
+    let mut robot = init_peripherals(chip, cortex);
+    let mut eth = W5500::new(&mut robot.spi_eth, &mut robot.cs);
+    init_eth(
+        &mut eth,
+        &mut robot.spi_eth,
+        &MacAddress::new(0x02, 0x01, 0x02, 0x03, 0x04, 0x05),
+        &IpAddress::new(192, 168, 0, 222),
+    );
 
-    let mut dbg = bluepill.DBG;
+    let mut buffer = [0; 2048];
 
-    // Config des horloges
-    let mut rcc = bluepill.RCC.constrain();
-    let mut flash = bluepill.FLASH.constrain();
-    let mut afio = bluepill.AFIO.constrain(&mut rcc.apb2);
+    loop {
+        if let Some((_, _, size)) = eth
+            .try_receive_udp(&mut robot.spi_eth, SOCKET_UDP, &mut buffer)
+            .unwrap()
+        {
+            let _id = buffer[0];
 
-    let clocks = rcc
-        .cfgr /*
-        .use_hse(8.mhz())
-        .sysclk(72.mhz())
-        .pclk1(36.mhz())
-        .pclk2(72.mhz())*/
-        .freeze(&mut flash.acr);
-
-    // Config du GPIO
-    let mut gpiob = bluepill.GPIOB.split(&mut rcc.apb2);
-
-    loop {}
-}
-
-//  For any hard faults, show a message on the debug console and stop.
-
-#[interrupt]
-fn TIM3() {
-    static mut HIGH_CAPTURE: bool = false;
-    static mut HIGH_ARR: bool = false;
-    unsafe {
-        let capture = (*f103::TIM3::ptr()).sr.read().cc1if();
-
-        if capture.bit_is_set() {
-            if *HIGH_CAPTURE {
-                (*f103::GPIOB::ptr()).bsrr.write(|w| w.br13().set_bit());
-            } else {
-                (*f103::GPIOB::ptr()).bsrr.write(|w| w.bs13().set_bit());
+            match IO::from_json_slice(&buffer[1..size]) {
+                Ok(_e) => {}
+                Err(e) => panic!("{:#?}", e),
             }
-            (*f103::TIM3::ptr()).sr.write(|w| w.cc1if().clear_bit());
-            *HIGH_CAPTURE = !(*HIGH_CAPTURE);
-        } else {
-            if *HIGH_ARR {
-                (*f103::GPIOB::ptr()).bsrr.write(|w| w.br14().set_bit());
-            } else {
-                (*f103::GPIOB::ptr()).bsrr.write(|w| w.bs14().set_bit());
-            }
-            *HIGH_ARR = !(*HIGH_ARR);
         }
-        (*f103::TIM3::ptr()).sr.write(|w| w.uif().clear_bit());
     }
-}
-
-#[exception]
-fn HardFault(ef: &ExceptionFrame) -> ! {
-    asm::bkpt();
-    panic!("Hard fault: {:#?}", ef);
-}
-
-//  For any unhandled interrupts, show a message on the debug console and stop.
-
-#[exception]
-fn DefaultHandler(irqn: i16) {
-    asm::bkpt();
-    panic!("Unhandled exception (IRQn = {})", irqn);
 }
---
+++ src/robot.rs
@@ -0,0 +1,145 @@
+use crate::f103;
+use crate::f103::interrupt;
+use crate::f103::Peripherals;
+use crate::f103::SPI1;
+use crate::hal::delay::Delay;
+use crate::hal::gpio::{
+    gpioa::*, gpiob::*, gpioc::*, Alternate, Floating, Input, Output, PushPull,
+};
+use crate::hal::prelude::*;
+
+use crate::hal::spi::*;
+use crate::hal::timer::Timer;
+use crate::CortexPeripherals;
+use cortex_m_rt::exception;
+use cortex_m_rt::ExceptionFrame; //  Stack frame for exception handling.
+
+type SpiPins = (
+    PA5<Alternate<PushPull>>,
+    PA6<Input<Floating>>,
+    PA7<Alternate<PushPull>>,
+);
+
+pub struct Robot<K, P> {
+    pub spi_eth: Spi<K, P>,
+    pub delay: Delay,
+    pub led_black_pill: PC13<Output<PushPull>>,
+    pub pumps: PB0<Output<PushPull>>,
+    pub vannes: [PBx<Output<PushPull>>; 8],
+    pub cs: PB13<Output<PushPull>>,
+}
+
+pub fn init_peripherals(chip: Peripherals, cortex: CortexPeripherals) -> Robot<SPI1, SpiPins> {
+    //  Get the clocks from the STM32 Reset and Clock Control (RCC) and freeze the Flash Access Control Register (ACR).
+    let _dbg = chip.DBG;
+    // Config des horloges
+    let mut rcc = chip.RCC.constrain();
+    let mut flash = chip.FLASH.constrain();
+    let mut afio = chip.AFIO.constrain(&mut rcc.apb2);
+
+    let clocks = rcc
+        .cfgr
+        .use_hse(8.mhz())
+        .sysclk(72.mhz())
+        .pclk1(36.mhz())
+        .pclk2(72.mhz())
+        .freeze(&mut flash.acr);
+
+    //  Configuration des GPIOs
+    let mut gpioa = chip.GPIOA.split(&mut rcc.apb2);
+    let mut gpiob = chip.GPIOB.split(&mut rcc.apb2);
+    let mut gpioc = chip.GPIOC.split(&mut rcc.apb2);
+
+    // Configuration des PINS
+
+    // Slave select, on le fixe à un état bas (on n'en a pas besoin, une seule communication)
+    let mut cs = gpiob.pb13.into_push_pull_output(&mut gpiob.crh);
+    cs.set_low();
+
+    let sclk = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
+    let miso = gpioa.pa6.into_floating_input(&mut gpioa.crl);
+    let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
+
+    let vannes = [
+        gpiob.pb3.into_push_pull_output(&mut gpiob.crl).downgrade(),
+        gpiob.pb4.into_push_pull_output(&mut gpiob.crl).downgrade(),
+        gpiob.pb5.into_push_pull_output(&mut gpiob.crl).downgrade(),
+        gpiob.pb6.into_push_pull_output(&mut gpiob.crl).downgrade(),
+        gpiob.pb8.into_push_pull_output(&mut gpiob.crh).downgrade(),
+        gpiob.pb12.into_push_pull_output(&mut gpiob.crh).downgrade(),
+        gpiob.pb14.into_push_pull_output(&mut gpiob.crh).downgrade(),
+        gpiob.pb15.into_push_pull_output(&mut gpiob.crh).downgrade(),
+    ];
+
+    let pumps = gpiob.pb0.into_push_pull_output(&mut gpiob.crl);
+
+    {
+        // Hardfault LED
+        let mut pin = gpiob.pb7.into_push_pull_output(&mut gpiob.crl);
+        pin.set_low();
+        // Blinking led
+        let mut led = gpioc.pc14.into_push_pull_output(&mut gpioc.crh);
+        led.set_low();
+    }
+    let led_black_pill = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+    let spi = Spi::spi1(
+        chip.SPI1,
+        (sclk, miso, mosi),
+        &mut afio.mapr,
+        Mode {
+            polarity: Polarity::IdleLow,
+            phase: Phase::CaptureOnFirstTransition,
+        },
+        1.mhz(),
+        clocks,
+        &mut rcc.apb2,
+    );
+
+    // Clignotement de la led
+    let _ = Timer::tim3(chip.TIM3, 5.hz(), clocks, &mut rcc.apb1);
+
+    //  Create a delay timer from the RCC clocks.
+    let delay = Delay::new(cortex.SYST, clocks);
+
+    Robot {
+        spi_eth: spi,
+        delay,
+        led_black_pill,
+        pumps,
+        vannes,
+        cs,
+    }
+}
+
+#[interrupt]
+fn TIM3() {
+    static mut TOOGLE: bool = false;
+    unsafe {
+        if *TOOGLE {
+            (*f103::GPIOC::ptr()).bsrr.write(|w| w.br14().set_bit());
+        } else {
+            (*f103::GPIOC::ptr()).bsrr.write(|w| w.bs14().set_bit());
+        }
+        *TOOGLE = !(*TOOGLE);
+        (*f103::TIM3::ptr()).sr.write(|w| w.uif().clear_bit());
+    }
+}
+
+#[exception]
+fn HardFault(ef: &ExceptionFrame) -> ! {
+    unsafe {
+        (*f103::GPIOB::ptr()).bsrr.write(|w| w.br7().set_bit());
+    }
+    panic!("Hard fault: {:#?}", ef);
+}
+
+//  For any unhandled interrupts, show a message on the debug console and stop.
+
+#[exception]
+fn DefaultHandler(irqn: i16) {
+    unsafe {
+        (*f103::GPIOB::ptr()).bsrr.write(|w| w.br7().set_bit());
+    }
+    panic!("Unhandled exception (IRQn = {})", irqn);
+}
